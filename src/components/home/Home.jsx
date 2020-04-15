7ce16296807d8544468026b2e8273456e7871621
Louis Collet <colletlouis@gmail.com>
2020-04-15 14:07:25 +0200

    Home page first part done

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import Footer from '../nav/Footer';
+import SHBSLong from '../../img/SHBSLong.png';
+import playnowtext from '../../img/playnowtext.png';
+import arrowswhite from '../../img/arrowswhite.png';
 import './Home.css';
 
-function Home() {
+const Home = () => {
   return (
-    <div>
-      <h1>Page accueil</h1>
-      <Footer />
+    <div className="home-main">
+      <div className="home-first-bg">
+        <div className="home-assets">
+          <img
+            src={SHBSLong}
+            alt="SuperHeroes Battle Simulator's title"
+            className="home-shbs-title"
+          />
+          <div className="home-playnow-button">
+            <img src={playnowtext} alt="Play now button text" />
+          </div>
+          <img src={arrowswhite} alt="Arrows to scroll down" className="home-arrows" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
