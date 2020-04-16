e1f7f3872557c6ef6da3868635e34f6e88bd3a01
PaulCrtr <paul.th.cartier@gmail.com>
2020-04-16 18:31:28 +0200

    experiment auto-scroll

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,33 +6,53 @@ import playnowtext from '../../img/playnowtext.png';
 import arrowswhite from '../../img/arrowswhite.png';
 import './Home.css';
 
-const Home = () => {
-  return (
-    <div className="home-main">
-      <NavBar />
-      <div className="home-first-bg">
-        <div className="home-assets">
-          <img
-            src={SHBSLong}
-            alt="SuperHeroes Battle Simulator's title"
-            className="home-shbs-title"
-          />
-          <Link to="Collection" className="home-playnow-button">
-            <img src={playnowtext} alt="Play now button text" />
-          </Link>
-          <img src={arrowswhite} alt="Arrows to scroll down" className="home-arrows" />
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+    this.state = { scrollTop: 0 };
+    this.onScroll = this.onScroll.bind(this);
+  }
+
+  onScroll() {
+    const { scrollY } = window; // Don't get confused by what's scrolling - It's not the window
+    const { scrollTop } = this.myRef.current;
+    // eslint-disable-next-line no-console
+    console.log(`onScroll, window.scrollY: ${scrollY} myRef.scrollTop: ${scrollTop}`);
+    // eslint-disable-next-line object-shorthand
+    this.setState({ scrollTop: scrollTop });
+  }
+
+  render() {
+    const { scrollTop } = this.state;
+    return (
+      <div className="home-main" ref={this.myRef} onScroll={this.onScroll}>
+        <p>{scrollTop}</p>
+        <NavBar />
+        <div className="home-first-bg">
+          <div className="home-assets">
+            <img
+              src={SHBSLong}
+              alt="SuperHeroes Battle Simulator's title"
+              className="home-shbs-title"
+            />
+            <Link to="Collection" className="home-playnow-button">
+              <img src={playnowtext} alt="Play now button text" />
+            </Link>
+            <img src={arrowswhite} alt="Arrows to scroll down" className="home-arrows" />
+          </div>
+        </div>
+        <div className="home-second-page">
+          <div>
+            <img
+              src="https://via.placeholder.com/720x480.png?text=Board Presentation"
+              alt="Placeholder"
+            />
+          </div>
         </div>
       </div>
-      <div className="home-second-page">
-        <div>
-          <img
-            src="https://via.placeholder.com/720x480.png?text=Board Presentation"
-            alt="Placeholder"
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Home;
