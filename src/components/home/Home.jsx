0fd6cf5dd81dcaf3f0436636c41d2c02aff7210f
PaulCrtr <paul.th.cartier@gmail.com>
2020-04-16 20:38:55 +0200

    scroll animation quite advenced

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,25 +9,26 @@ import './Home.css';
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.myRef = React.createRef();
-    this.state = { scrollTop: 0 };
-    this.onScroll = this.onScroll.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.scrollRef = React.createRef();
   }
 
-  onScroll() {
-    const { scrollY } = window; // Don't get confused by what's scrolling - It's not the window
-    const { scrollTop } = this.myRef.current;
-    // eslint-disable-next-line no-console
-    console.log(`onScroll, window.scrollY: ${scrollY} myRef.scrollTop: ${scrollTop}`);
-    // eslint-disable-next-line object-shorthand
-    this.setState({ scrollTop: scrollTop });
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    this.scrollRef.current.click();
+    console.log('ok');
   }
 
   render() {
-    const { scrollTop } = this.state;
     return (
-      <div className="home-main" ref={this.myRef} onScroll={this.onScroll}>
-        <p>{scrollTop}</p>
+      <div className="home-main" onScroll={this.handleScroll}>
         <NavBar />
         <div className="home-first-bg">
           <div className="home-assets">
@@ -39,10 +40,12 @@ class Home extends React.Component {
             <Link to="Collection" className="home-playnow-button">
               <img src={playnowtext} alt="Play now button text" />
             </Link>
-            <img src={arrowswhite} alt="Arrows to scroll down" className="home-arrows" />
+            <a href="#bottom-encre" ref={this.scrollRef}>
+              <img src={arrowswhite} alt="Arrows to scroll down" className="home-arrows" />
+            </a>
           </div>
         </div>
-        <div className="home-second-page">
+        <div id="bottom-encre" className="home-second-page">
           <div>
             <img
               src="https://via.placeholder.com/720x480.png?text=Board Presentation"
