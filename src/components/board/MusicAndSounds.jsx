99aedee0bf15f418db728361fb7e525265877530
PaulCrtr <paul.th.cartier@gmail.com>
2020-05-11 13:27:32 +0200

    music start when board mount, sound can be muted

--- src/components/board/MusicAndSounds.jsx
+++ src/components/board/MusicAndSounds.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import music from '../../audio/music.mp3';
+import musicOn from '../../img/musicOn.png';
+import musicOff from '../../img/musicOff.png';
 
 const MusicAndSounds = () => {
+  const [soundActive, setSoundActive] = useState(true);
+
   return (
     <div>
-      <audio autoPlay controls src={music}></audio>
+      {soundActive ? (
+        <>
+          <img
+            className="active-sound"
+            onClick={() => setSoundActive(false)}
+            src={musicOn}
+            alt="Sound on"
+          />
+          <audio autoPlay loop src={music} />
+        </>
+      ) : (
+        <>
+          <img
+            className="active-sound"
+            onClick={() => setSoundActive(true)}
+            src={musicOff}
+            alt="Sound off"
+          />
+          <audio autoPlay muted loop src={music} />
+        </>
+      )}
     </div>
   );
 };
