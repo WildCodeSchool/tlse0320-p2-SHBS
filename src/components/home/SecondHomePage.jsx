d6e87bc17e8c5b5faaf503cdb65759671b58864e
Louis <colletlouis@gmail.com>
2020-05-13 12:06:26 +0200

    Text on home page

--- src/components/home/SecondHomePage.jsx
+++ src/components/home/SecondHomePage.jsx
@@ -8,15 +8,11 @@ const SecondHomePage = () => {
           src="https://via.placeholder.com/720x480.png?text=Board Presentation"
           alt="Placeholder"
         />
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur officia minima
-          saepe earum officiis facere libero numquam voluptas quasi, impedit alias maxime nisi quas
-          fugiat rem nobis provident. Ab? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Aliquam voluptate, consequuntur natus unde quos perferendis delectus dolor eveniet
-          veritatis doloribus commodi possimus expedita deserunt. Facilis veritatis sapiente quaerat
-          officia tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio qui vel
-          fugiat sit et nisi, eligendi, accusantium atque id voluptatum amet deleniti ipsa
-          asperiores eaque odit quo quisquam, corporis quidem!
+        <p className="bigger-P-Li">
+          {`Did you ever argued with you friends to be able to state who's the strongest superhero ?
+          Of course you did, we all did. But who's right ? 
+          Well, SuperHeroes Battle Simulator is the answer ! Choose your dream team between heroes
+          from multiple universes and fight your opponents in this turn-based card game.`}
         </p>
       </article>
       <article className="flex-row">
@@ -24,15 +20,13 @@ const SecondHomePage = () => {
           src="https://via.placeholder.com/720x480.png?text=Cards Presentation"
           alt="Placeholder"
         />
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil consectetur officia minima
-          saepe earum officiis facere libero numquam voluptas quasi, impedit alias maxime nisi quas
-          fugiat rem nobis provident. Ab? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Aliquam voluptate, consequuntur natus unde quos perferendis delectus dolor eveniet
-          veritatis doloribus commodi possimus expedita deserunt. Facilis veritatis sapiente quaerat
-          officia tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio qui vel
-          fugiat sit et nisi, eligendi, accusantium atque id voluptatum amet deleniti ipsa
-          asperiores eaque odit quo quisquam, corporis quidem!
+        <p className="bigger-P-Li">
+          {`Rules are pretty simples: each turn, you can select a card among you three heroes and
+          choose who youwant to attack. The number on the left is the total of damages you're gonna
+          inflict to your opponent. On the right, it's the remaining health of the hero. If the life
+          is down to "0", the hero is out for the game. Your goal is to keep your team alive
+          while you get rid of your opponents. But be carefull when attacking, your opponent will
+          fight back...`}
         </p>
       </article>
     </section>
